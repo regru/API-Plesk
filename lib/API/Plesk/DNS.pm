805d87ae97ad7af2b017c400104488955480acf3
vaneska <vaneska@774be9cd-a8d6-4ce2-965c-ab2e9856cb01>
2011-06-01 19:58:20 +0000

    added DNS

--- lib/API/Plesk/DNS.pm
+++ lib/API/Plesk/DNS.pm
@@ -8,4 +8,22 @@ use Carp;
 
 use base 'API::Plesk::Component';
 
+sub add_rec {
+    my ( $self, %params ) = @_;
+    my $bulk_send = delete $params{bulk_send};
+
+    my @fields = (
+        [qw(site-id site-alias-id)],
+        'type',
+        'host',
+        'value'
+    );
+
+    $self->check_required_params(\%params, @fields);
+    my $data = $self->sort_params(\%params, @fields, 'opt');
+
+    return $bulk_send ? $data : 
+        $self->plesk->send('site', 'get', $data);
+}
+
 1;
