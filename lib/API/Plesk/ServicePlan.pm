0c737976f9ec79e81b9ada24f16d41c751b8f9f6
vaneska <vaneska@774be9cd-a8d6-4ce2-965c-ab2e9856cb01>
2011-07-22 10:46:03 +0000

    added set and del operations to service-plan

--- lib/API/Plesk/ServicePlan.pm
+++ lib/API/Plesk/ServicePlan.pm
@@ -5,8 +5,23 @@ use strict;
 use warnings;
 
 use Carp;
-
+use Data::Dumper;
 use base 'API::Plesk::Component';
+
+my @main_fields = qw(
+    owner-id
+    owner-login
+    mail
+    limits
+    log-rotation
+    preferences
+    hosting
+    performance
+    permissions
+    external-id
+    name
+);
+
 
 sub get {
     my ($self, %filter) = @_;
@@ -20,6 +35,34 @@ sub get {
         $self->plesk->send('service-plan', 'get', $data);
 }
 
+sub set {
+    my ( $self, %params ) = @_;
+    my $bulk_send = delete $params{bulk_send}; 
+    my $filter    = delete $params{filter} || '';
+    
+    $self->check_hosting(\%params);
+
+    my $data = [
+        { filter  => $filter },
+        @{$self->sort_params(\%params, @main_fields)},
+    ];
+
+    return $bulk_send ? $data : 
+        $self->plesk->send('service-plan', 'set', $data);
+}
+
+sub del {
+    my ($self, %filter) = @_;
+    my $bulk_send = delete $filter{bulk_send}; 
+
+    my $data = {
+        filter  => @_ > 2 ? \%filter : ''
+    };
+
+    return $bulk_send ? $data : 
+        $self->plesk->send('service-plan', 'del', $data);
+}
+
 1;
 
 __END__
@@ -31,7 +74,9 @@ API::Plesk::ServicePlan -  Managing service plans.
 =head1 SYNOPSIS
 
     $api = API::Plesk->new(...);
-    $response = $api->service_plan->get(..);
+    $response = $api->service_plan->get(...);
+    $response = $api->service_plan->set(...);
+    $response = $api->service_plan->del(...);
 
 =head1 DESCRIPTION
 
@@ -42,6 +87,10 @@ Module manage service plans.
 =over 3
 
 =item get(%params)
+
+=item setarams)
+
+=item del(%params)
 
 =back
 
