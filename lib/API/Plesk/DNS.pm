5999e6b192b93572cc8d32ed4bda8eb3823c45a7
Anton Gerasimov <a.gerasimov@reg.ru>
2014-07-23 16:34:20 +0600

    fixes for get_soa() and dns.t tests

--- lib/API/Plesk/DNS.pm
+++ lib/API/Plesk/DNS.pm
@@ -22,7 +22,7 @@ sub add_rec {
     $self->check_required_params(\%params, @fields);
     my $data = $self->sort_params(\%params, @fields, 'opt');
 
-    return $bulk_send ? $data : 
+    return $bulk_send ? $data :
         $self->plesk->send('dns', 'add_rec', $data);
 }
 
@@ -30,14 +30,14 @@ sub get_rec {
     my ( $self, %filter ) = @_;
     my $bulk_send = delete $filter{bulk_send};
     my $template = delete $filter{template};
-    
+
 
     my $data = [
         { filter  => @_ > 2 ? \%filter : '' },
         ( $template ? {template => $template} : () ),
     ];
 
-    return $bulk_send ? $data : 
+    return $bulk_send ? $data :
         $self->plesk->send('dns', 'get_rec', $data);
 }
 
@@ -51,7 +51,7 @@ sub del_rec {
         ( $template ? {template => $template} : () ),
     ];
 
-    return $bulk_send ? $data : 
+    return $bulk_send ? $data :
         $self->plesk->send('dns', 'del_rec', $data);
 }
 
@@ -59,11 +59,12 @@ sub get_soa {
     my ( $self, %filter ) = @_;
     my $bulk_send = delete $filter{bulk_send};
 
-    my $data = [
-        { filter  => @_ > 2 ? \%filter : '' }
-    ];
+    my $data = {
+        filter  => @_ > 2 ? \%filter : '',
+        soa     => '',
+    };
 
-    return $bulk_send ? $data : 
+    return $bulk_send ? $data :
         $self->plesk->send('dns', 'get', $data);
 }
 
--- t/dns.t
+++ t/dns.t
@@ -10,12 +10,24 @@ use Data::Dumper;
 use lib 't';
 use TestData;
 
-BEGIN { 
-    plan tests => 2;
-    use_ok('API::Plesk::DNS'); 
+BEGIN {
+    plan tests => 4;
+    use_ok('API::Plesk::DNS');
 }
 
 my $api = API::Plesk->new( %TestData::plesk_valid_params );
 
+my $dns = API::Plesk::DNS->new( plesk => $api );
+
+isa_ok($dns, 'API::Plesk::DNS');
+
 isa_ok($api->dns, 'API::Plesk::DNS');
 
+is_deeply(
+    $dns->get_soa(id => 1, bulk_send => 1),
+    {
+        filter  => { id => 1 },
+        soa     => '',
+    },
+    'get_soa'
+);
